25aa2de89d2cfb5a0deb56615bc6121ce03e22be
Kate Howard <k.l.howard.1992@gmail.com>
2020-11-28 12:14:24 +0000

    Display first row

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,40 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Seat v-for="(seat, index) in seats" :key="index" :seat="seat"></Seat>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Seat from './components/Seat.vue'
 
 export default {
   name: 'App',
+  data() {
+    return {
+      seats: Array.from(Array(32).keys()).reverse().map(n => `A${n+1}`)
+    }
+  },
   components: {
-    HelloWorld
+    Seat
   }
 }
 </script>
 
 <style>
+body {
+  padding: 0;
+  background-color: #ffffcc;
+  display: flex;
+  justify-content: center;
+  margin: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: grid;
+  margin: 40px 0;
+  grid-template-columns: repeat(32, 30px);
+  grid-template-rows: repeat(8, 30px);
+  grid-gap: 5px;
 }
 </style>
---
+++ src/components/Seat.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="seat">
+    {{ seat }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Seat',
+  props: { seat: String }
+}
+</script>
+
+<style scoped>
+  .seat {
+    width: 30px;
+    height: 30px;
+    background-color: #cc3300;
+    color: #ffffcc;
+    border-top-left-radius: 10px;
+    border-top-right-radius: 10px;
+  }
+</style>
